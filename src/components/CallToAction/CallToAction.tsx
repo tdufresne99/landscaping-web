18171e2815a072eb4b3e6ce15973b00823d2cbbe
Thomas Dufresne <tdufresne99@gmail.com>
2025-07-05 12:48:57 -0400

    fixed cursor pointer on buttons

--- src/components/CallToAction/CallToAction.tsx
+++ src/components/CallToAction/CallToAction.tsx
@@ -9,7 +9,15 @@ interface CallToActionProps {
   buttonLabel: string;
   buttonLink: string;
   className?: string;
-  buttonVariant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "dark" | "light";
+  buttonVariant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "dark"
+    | "light";
   buttonSize?: "default" | "sm" | "lg" | "icon";
 }
 
@@ -17,20 +25,22 @@ export default function CallToAction({
   title,
   subtitle,
   buttonLabel,
-  buttonLink,
+  buttonLink = "#",
   className = "text-theme-light text-center bg-theme-dark",
   buttonVariant = "default",
   buttonSize = "default",
 }: CallToActionProps) {
   return (
-    <div
-      className={`w-full py-4 px-8 flex flex-col ${className}`}
-    >
+    <div className={`py-4 px-8 flex flex-col ${className}`}>
       <h2 className="text-lg font-semibold mb-4">{title}</h2>
       {subtitle && <p className="text-md mb-6">{subtitle}</p>}
-      <Link href={buttonLink}>
-        <Button variant={buttonVariant} size={buttonSize}>{buttonLabel}</Button>
-      </Link>
+      <div>
+        <Link href={buttonLink}>
+          <Button variant={buttonVariant} size={buttonSize}>
+            {buttonLabel}
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
